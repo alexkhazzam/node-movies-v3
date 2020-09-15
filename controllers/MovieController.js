c5a563796258732ab90ef1512b98e76c9b6e7f6a
alexkhazzam <alex722khazzam@gmail.com>
2020-09-15 18:13:54 -0400

    adding stuff

--- controllers/MovieController.js
+++ controllers/MovieController.js
@@ -1,14 +1,58 @@
 const fs = require('fs');
 
-exports.getMovie = (req, res) => {
-  console.log(`[ Get Movie ] ${req.requestTime}`);
-  res.status(200).json({
-    status: 'success',
+const fetchMovies = (file) => {
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf-8', (err, data) => {
+      if (err) {
+        reject('Could not find file!');
+      } else {
+        resolve(JSON.parse(data));
+      }
+    });
   });
+  return promise;
 };
 
-exports.deleteMovie = (req, res) => {
-  console.log(`[ Delete Movie | id: ${req.params.id}] ${req.requestTime}`);
+exports.getMovie = (req, res) => {
+  console.log(`[ Get Movie | id: ${req.params.id}] | ${req.requestTime}`);
+  fetchMovies(`${__dirname}/../data/movies.json`)
+    .then((data) => {
+      const movie = data.find((movieEl) => movieEl.id === req.params.id * 1);
+      res.status(200).json({
+        status: 'success',
+        data: {
+          movie,
+        },
+      });
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+exports.deleteMovie = async (req, res) => {
+  console.log(`[ Delete Movie | id: ${req.params.id}] | ${req.requestTime}`);
+  try {
+    const movies = await fetchMovies(`${__dirname}/../data/movies.json`);
+    const deletedMov = movies.find(
+      (movieEl) => movieEl.id === req.params.id * 1
+    );
+    fs.writeFileSync(
+      `${__dirname}/../data/deletedMovies.json`,
+      JSON.stringify(deletedMov)
+    );
+
+    movies.splice([req.params.id - 1], 1);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        deleted: deletedMov,
+        movies: movies,
+      },
+    });
+  } catch (err) {
+    throw err;
+  }
 };
 
 exports.addMovie = (req, res) => {
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const app = require('./app');
-
 const port = 5000;
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
