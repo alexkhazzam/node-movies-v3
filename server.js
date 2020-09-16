2f4eb38b05a767ae81c5a14c85b3252918902dfe
alexkhazzam <alex722khazzam@gmail.com>
2020-09-16 07:05:52 -0400

    adding stuf

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+const dotenv = require('dotenv');
 const app = require('./app');
-const port = 5000;
+
+dotenv.config({ path: `./config.env` });
+
+const port = process.env.PORT || 3000;
+console.log(process.env);
+
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
