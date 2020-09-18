de5bc01cf5c8f6128adf9067f199b972d42f31d9
alexkhazzam <65082946+alexkhazzam@users.noreply.github.com>
2020-09-18 08:18:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ dotenv.config({ path: `./config.env` });
 
 const port = process.env.PORT || 3000;
 console.log(process.env);
+console.log(process.env.USER_NAME);
 
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
